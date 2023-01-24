c8db97bdfb3f9d452368fc3b918c54190f154fe1
Onuralp Ozkan <onuralpozkan01@gmail.com>
2023-01-25 00:14:13 +0300

    cors config added urls

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,11 +1,12 @@
 const allowedOrigins = [
   process.env.ALLOWED_ORIGIN_1,
   process.env.ALLOWED_ORIGIN_2,
-  process.env.ALLOWED_ORIGIN_3
+  process.env.ALLOWED_ORIGIN_3,
+  process.env.ALLOWED_ORIGIN_4,
+  process.env.ALLOWED_ORIGIN_5
 ];
 var corsOptions = {
   origin: function (origin, callback) {
-    console.log("origin", origin);
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
